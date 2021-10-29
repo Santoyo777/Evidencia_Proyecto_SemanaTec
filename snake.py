d4199de0787c28e5650b99d8697b0a3b3d961cd5
Luisjakg <52958375+Luisjakg@users.noreply.github.com>
2021-10-28 23:05:35 -0500

    Update snake.py

--- snake.py
+++ snake.py
@@ -13,15 +13,19 @@ from turtle import *
 from random import randrange
 from freegames import square, vector
 
+
+#Declaracion de Variables
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
 
+#Declaracion de funciones basicas
 def change(x, y):
     "Change snake direction."
     aim.x = x
     aim.y = y
 
+#Revisa si la cabeza de la serpiente esta dentro del cuadro
 def inside(head):
     "Return True if head inside boundaries."
     return -200 < head.x < 190 and -200 < head.y < 190
@@ -32,6 +36,7 @@ def move_food():
     food.y = randrange (-15,15) *10
     ontimer(move_food,5500)
 
+#Funcion para recorrer la serpiente por un cuadro
 def move():
     "Move snake forward one segment."
     head = snake[-1].copy()
@@ -66,7 +71,8 @@ def move():
     #Changing 100 to 25 to increase snake speed
     ontimer(move, 25)
 
-
+#Ejecucion
+#Se genera el cuadro de juego
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
